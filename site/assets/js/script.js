a2d88c66b861ab06ecf6719222ac93658d418cb8
Julio Canizalez <juliocanizalez@outlook.com>
2021-01-28 00:54:11 -0600

    hot fix

--- site/assets/js/script.js
+++ site/assets/js/script.js
@@ -2,7 +2,6 @@ const KEY = 'PfiUDKrZ2OcdQLZqVdBfb656Hb0aWLu6zDXedMZ8';
 const URL = 'https://api.nasa.gov/planetary/earth/assets?';
 var imgUrl;
 
-
 let textfield = document.getElementById("text");
 
 textfield.addEventListener("keydown", (e) => {
@@ -16,22 +15,9 @@ textfield.addEventListener("keydown", (e) => {
         let latitude = values[0];
 
         fetch(`${URL}lon=${longitude}&lat=${latitude}&date=2018-01-01&dim=0.05&api_key=${KEY}`)
-        .then(response=> {
-            if(response.ok){
-                response.json()
-            }else{
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Cannot get your image :(',
-                    footer:'Try latitude,longitude',
-                    showConfirmButton: false,
-                    showCloseButton: true                  
-                })
-            }
-            
-        })
-        .then(data => {
+        .then(response=> response.json())
+        .then((data) => {
+            console.log(data)
             document.getElementById("img").innerHTML = `<img src="${data.url}" class="img-fluid"></img>`;
         })  
     }
